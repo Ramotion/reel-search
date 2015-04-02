40b82eda2ee02c148ce49d74c760cd8a10a9db32
Mikhail Stepkin <mikhail.s@ramotion.com>
2015-04-02 12:24:27 +0300

    Initial Commit

---
+++ RAMReel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RAMReel
+//
+//  Created by Mikhail Stepkin on 4/2/15.
+//  Copyright (c) 2015 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
